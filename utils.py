f4b9b5d16aa477bdbec289fddea16afe65619f9f
Ranley Yu <ranley@Ranleys-MacBook-Pro.local>
2020-09-11 03:10:43 +0800

    first

--- utils.py
+++ utils.py
@@ -1,13 +1,12 @@
 import os
 from glob import glob
-from constants import *
 from pathlib import Path
 import shutil
 from pydub import AudioSegment
 
 def find_files(dir, pattern="*.wav"):
     files = []
-    for path in Path(directory).rglob(pattern):
+    for path in Path(dir).rglob(pattern):
         files.append(path)
     return files
 
@@ -19,9 +18,23 @@ def extract_wav(in_dir, out_dir):
 def get_audio_duration(filename):
     sound= AudioSegment.from_wav(filename)
     duration = sound.duration_seconds
-    return duration
+    return round(duration,4)
 
-def make_wav_scp(wave_dir = WAV_DIR):
+def load_n_col(filename, first_title=True, numpy=False):
+    data = []
+    with open(filename) as fp:
+        for line in fp:
+            data.append(line.strip().split())
+    if first_title:
+        data = data[1:]
+    columns = list(zip(*data))
+    if numpy:
+        columns = [np.array(list(i)) for i in columns]
+    else:
+        columns = [list(i) for i in columns]
+    return columns
+
+def make_wav_scp(wave_dir):
     files = find_files(wave_dir)
     f = open("wav.scp", "w")
     for path in files:
@@ -31,18 +44,26 @@ def make_wav_scp(wave_dir = WAV_DIR):
         f.write(recording_id + " " + file_path + "\n")
     f.close()
 
-def make_segments(wave_dir = WAV_DIR):
-    files = find_files(wave_dir)
-    f = open("segments", "w")
-    for path in files:
+def make_segments(wav_dir, out_dir):
+    files = find_files(wav_dir)
+    cols = load_n_col(os.path.join(wav_dir,"train.txt"))
+    assert(len(files) == len(cols[0]))
+
+    f = open(os.path.join(out_dir, "segments"), "w")
+    for i in range(len(files)):
+        path = files[i]
         uttid = path.name.split('.')[0]
-        spkid = path.name.split('.')[0].split('-')[0]
+        spkid = ""
+        if int(cols[1][i]) == 1:
+            spkid = "fake"
+        else:
+            spkid = cols[2][i]
         duration = get_audio_duration(path)
         
         f.write("{} {} 0.00 {}\n".format(uttid, spkid, str(duration)))
     f.close()
 
-def make_utt2spk(wave_dir = WAV_DIR):
+def make_utt2spk(wave_dir):
     files = find_files(wave_dir)
     # print(files)
     f = open("utt2spk", "w")
@@ -52,3 +73,6 @@ def make_utt2spk(wave_dir = WAV_DIR):
 
         f.write(utter_id + " " + recording_id + "\n")
     f.close()
+
+if __name__ == "__main__":
+    make_segments("/Users/ranley/Downloads/train")
